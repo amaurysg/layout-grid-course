4010693b6f84831bdf0c3567abfe507d96121b17
Amaury Soto <amaurysotog@gmail.com>
2020-07-25 19:18:27 -0500

    Layout Cartman Responsive!

---
+++ moneybox.js
@@ -0,0 +1,20 @@
+function moneyBox(coins) {
+  var saveCoins = 0;
+  saveCoins += coins;
+  console.log(saveCoins);
+}
+moneyBox(20);
+moneyBox(40);
+
+const moneyBox = () => {
+  var saveCoins = 0;
+  const countMoney = (coins) => {
+    saveCoins += coins;
+    console.log(saveCoins);
+  };
+  return countMoney;
+};
+
+let total = moneyBox();
+total(10);
+total(20);
